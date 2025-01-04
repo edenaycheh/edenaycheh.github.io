88c5a2654769cdfac489d998a5742a970e142ed9
edenaycheh <edenaycheh@mail.tau.ac.il>
2025-01-04 20:55:51 +0200

    new links

---
+++ twist/tast_8/script.js
@@ -0,0 +1,8 @@
+
+function divClicked() {  
+    alert("The function is working!");
+
+    const dorothy = document.getElementById("dorothy");
+    dorothy.style.left = "300px";
+    dorothy.style.top = "400px";
+}
